fcb388b9b88566c14eef21cc0b5ab995427b4a41
Kat-Key <katkey706@gmail.com>
2021-02-18 15:13:11 -0500

    Switching ideas - change to calculate the number of evictions per zip code

--- evictions-permits-join.R
+++ evictions-permits-join.R
@@ -19,33 +19,18 @@ evictions <- str_glue('{data_dir}/Evictions.csv') %>%
   clean_names()
 
 
-#Read in permits from 1989 - 2013
-DOB_permits_historical <- str_glue('{data_dir}/Historical_DOB_Permit_Issuance.csv') %>%
-  read_csv() %>%
-  clean_names() %>%
-  select(c(borough, number, street, job_type, block, lot, postcode, residential, filing_date, owners_first_name, owners_last_name)) %>%
-  transmute(borough = borough, house_number = number, street_name = street, job_type = job_type,
-              block = block, lot = lot, zip_code = postcode, residential = residential, filing_date = filing_date, 
-              owners_first_name = owners_first_name, owners_last_name = owners_last_name)
+#Calculate number of evictions per zip code
+evictions_zip <- evictions %>%
+  group_by(eviction_zip) %>%
+  summarize(evictions_count = n())
 
-#Read in current permits
-DOB_permits <- str_glue('{data_dir}/DOB_Permit_Issuance.csv') %>%
-  read_csv() %>%
-  clean_names() %>%
-  select(c(borough, house_number, street_name, job_type, block, lot, zip_code, residential, filing_date, owners_first_name, owners_last_name))
+#Read in zip to zcta
 
+zcta_join <- str_glue('{data_dir}/zip_to_zcta10_nyc.csv') %>%
+  read_csv() 
 
+#Join evictions_zip and zcta_join to convert the zip codes to zctas
 
-#spec(DOB_permits)
-DOB_permits_all <- rbind(DOB_permits,DOB_permits_historical)
+eviction_zcta <- left_join(evictions_zip,zcta_join, by = c("eviction_zip" = "ZIP"))
 
-DOB_permits_all <- DOB_permits_all %>%
-  mutate(address = paste(house_number, street_name, sep = " ") ) %>%
-  filter(residential == "YES")
-
-sapply(DOB_permits_all, class)
-  
-evictions_small <- evictions %>%
-  select(c(eviction_address, eviction_apt_num, executed_date, borough, eviction_zip))
-
-test_join <- inner_join(evictions_small, DOB_permits_all, by = c("eviction_address" = "address", "eviction_zip" = "zip_code"))
+##TO-DO: Several are missing/incorrect so I will update and explore 
